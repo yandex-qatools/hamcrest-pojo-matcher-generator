ffbfad34f8722b081074391b7b2d25095d0beb17
Kirill Merkushev <lanwen@yandex.ru>
2016-03-10 15:24:42 +0300

    better test debug message

--- feature-matcher-generator-e2e-test/src/test/java/ru/yandex/qatools/proc/test/LordTest.java
+++ feature-matcher-generator-e2e-test/src/test/java/ru/yandex/qatools/proc/test/LordTest.java
@@ -3,9 +3,13 @@ package ru.yandex.qatools.proc.test;
 import org.hamcrest.Matcher;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.collection.IsArrayWithSize.arrayWithSize;
 
 /**
  * @author Vladislav Bauer
@@ -20,7 +24,10 @@ public class LordTest {
 
     @Test
     public void shouldFindMatchersForAllField() throws Exception {
-        assertThat(LordMatchers.class.getDeclaredMethods(), arrayWithSize(2));
+        assertThat(
+                Stream.of(LordMatchers.class.getDeclaredMethods()).map(Method::getName).collect(toList()), 
+                hasItems("withName", "withSlavesCount")
+        );
         assertThat(LordMatchers.class.getDeclaredMethod("withName", Matcher.class), notNullValue());
         assertThat(LordMatchers.class.getDeclaredMethod("withSlavesCount", Matcher.class), notNullValue());
     }
